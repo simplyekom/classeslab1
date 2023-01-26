68dd99357cba5d0d136423d577088b9df0abb710
simplyekom <119912241+simplyekom@users.noreply.github.com>
2023-01-25 20:50:56 -0700

    Add files via upload

---
+++ classeslab1/classeslab1/People/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classeslab1.Humans
+{
+    internal class Person
+    {
+        public int personId;
+        public string firstName;
+        public string lastName;
+        public string favoriteColour;
+        public int age;
+        public bool isWorking;
+
+        public Person(int personId, string firstName, string lastName, string favoriteColour, int age, bool isWorking)
+        {
+            this.personId = personId;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.favoriteColour = favoriteColour;
+            this.age = age;
+            this.isWorking = isWorking;
+        }
+
+        public void DisplayPersonInfo()
+        {
+            Console.WriteLine(personId + ": " + firstName + " " + lastName + "'s" + " " + "favorite color is" + " " + favoriteColour);
+        }
+
+        public override string ToString() 
+        {
+            return ($"PersonId: {personId}\nFirstName: {firstName}\nLastName: {lastName}\nFavoriteColour: {favoriteColour}\nAge: {age}\nIsWorking: {isWorking}");
+        }
+
+        public void ChangeFavoriteColour() 
+        {
+            favoriteColour = "White";
+            Console.WriteLine(personId + ": " + firstName + " " + lastName + "'s" + " " + "favorite color is:" + " " + favoriteColour);
+        }
+
+        public void GetAgeInTenYears()
+        {
+            age = age + 10;
+            Console.WriteLine(firstName + " " + lastName + "'s" + " Age in 10 year is: " + age);
+        }
+
+    }
+}
---
+++ classeslab1/classeslab1/Program.cs
@@ -0,0 +1,28 @@
+﻿using classeslab1.Humans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classeslab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1= new Person(1, "Ian", "Brooks", "Red", 30, true);
+            Person person2 = new Person(2, "Gina", "James", "Green", 18, false);
+            Person person3 = new Person(3, "Mike", "Briscoe", "Blue", 45, true);
+            Person person4 = new Person(4, "Mary", "Beals", "Yellow", 28, true);
+
+
+            person2.DisplayPersonInfo();
+            Console.WriteLine(person3);
+            person1.ChangeFavoriteColour();
+            person4.GetAgeInTenYears();
+            Console.ReadLine();
+
+        }
+    }
+}
